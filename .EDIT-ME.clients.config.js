7a3bb811a97f1516b44ab444f765fe3cb31d7ee9
Andrew Roberts <andrew.jacob.roberts@gmail.com>
2020-04-28 08:47:13 -0400

    Initial commit for revamp

--- .EDIT-ME.clients.config.js
+++
@@ -1,9 +0,0 @@
-export const clientConfig = {
-  ARDUINO_CHIP_ID: 123,
-  MQTT_HOST_URL: "tcp://localhost:1883",
-  MQTT_USERNAME: "web-dashboard",
-  MQTT_PASSWORD: "default",
-  SEMP_ENDPOINT: "http://localhost:8080/SEMP/v2/monitor",
-  SEMP_USERNAME: "admin",
-  SEMP_PASSWORD: "admin"
-};
